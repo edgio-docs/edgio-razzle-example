8aa0134cbf0e6c81a59ffd4a7bc8dbf2ff06cc6c
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2021-11-27 20:58:33 +0530

    Integrate E-Commerce Example

--- src/server.js
+++ src/server.js
@@ -6,18 +6,10 @@ import { renderToString } from 'react-dom/server'
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST)
 
-const cssLinksFromAssets = (assets, entrypoint) => {
-  return assets[entrypoint]
-    ? assets[entrypoint].css
-      ? assets[entrypoint].css.map(asset => `<link rel="stylesheet" href="${asset}">`).join('')
-      : ''
-    : ''
-}
-
 const jsScriptTagsFromAssets = (assets, entrypoint, extra = '') => {
   return assets[entrypoint]
     ? assets[entrypoint].js
-      ? assets[entrypoint].js.map(asset => `<script src="${asset}"${extra}></script>`).join('')
+      ? assets[entrypoint].js.map((asset) => `<script src="${asset}"${extra}></script>`).join('')
       : ''
     : ''
 }
@@ -39,19 +31,39 @@ server
     } else {
       res.status(200).send(
         `<!doctype html>
-    <html lang="">
-    <head>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta charset="utf-8" />
-        <title>Welcome to Razzle</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        ${cssLinksFromAssets(assets, 'client')}
-    </head>
-    <body>
-        <div id="root">${markup}</div>
-        ${jsScriptTagsFromAssets(assets, 'client', ' defer crossorigin')}
-    </body>
-</html>`
+          <html lang="">
+            <head>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+            <link
+              as="style"
+              rel="preload"
+              href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
+            />
+            <noscript>
+              <link
+                rel="stylesheet"
+                href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
+              />
+            </noscript>
+            <meta property="og:locale" content="en_US" />
+            <meta property="og:type" content="website" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta
+              name="robots"
+              content="max-snippet:-1, max-image-preview:large, max-video-preview:-1"
+            />
+            <meta charset="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+            <meta name="theme-color" content="#000000" />
+            <link rel="stylesheet" href="/index.css" />
+            </head>
+            <body>
+                <div id="root">${markup}</div>
+                ${jsScriptTagsFromAssets(assets, 'client', ' defer crossorigin')}
+                <script defer src="/__layer0__/devtools/install.js"></script>
+            </body>
+        </html>`
       )
     }
   })
